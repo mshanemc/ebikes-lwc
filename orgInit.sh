ccefe7b1b7a83b0add29e6d9cda938d73d7afcc2
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-11 08:33:06 -0600

    add init script

---
+++ orgInit.sh
@@ -0,0 +1,5 @@
+sfdx force:org:create -s -f config/project-scratch-def.json
+sfdx force:source:push
+sfdx force:user:permset:assign -n ebikes
+sfdx force:data:tree:import --plan ./data/sample-data-plan.json
+sfdx force:org:open -r lightning/n/Product_Explorer
